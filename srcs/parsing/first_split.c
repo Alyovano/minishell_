1f8948553bdf30f404c94afac421a1e8e18d1989
lturbang <lturbang@student.s19.be>
2020-10-21 09:43:41 +0200

    bug patch '\;' segfault in split

--- srcs/parsing/first_split.c
+++ srcs/parsing/first_split.c
@@ -35,9 +35,7 @@ int         maybe_split(t_user *start, int i)
         if (get_backslash(start->user_input, i) == 0)
             return (0);
     }
-    else 
-        return (1);
-    return (0);
+    return (1);
 }
 
 
