438106faa9ecbd0d80bfc871d5497c710e1dc5f4
lturbang <lturbang@student.s19.be>
2020-11-01 13:14:24 +0100

    add comment + clean, add flag management in execve, start proto dispatch_cmd function

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 10:14:42 by user42            #+#    #+#             */
-/*   Updated: 2020/11/01 11:00:10 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/01 13:12:00 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,31 @@
 // Fonction ayant pour unique but de tester
 // pour le moment
 
+/*
+** Ajout du flag dans execve pour tester le comportement
+*/
+
 int         petite_execution(t_list *lst, char **env)
 {
 	struct stat		test;
 	char			*path;
-	char 			*exec_arg[3];
+	char 			*exec_arg[4];
 
 	path = ft_strjoin("/bin/", lst->builtin);
 	if (stat(path, &test) != -1)
 	{
 		exec_arg[0] = lst->builtin;
- 		exec_arg[1] = lst->argu;
-		exec_arg[2] = NULL;
+		if (lst->flag)
+		{
+			exec_arg[1] = lst->flag;
+			exec_arg[2] = lst->argu;
+			exec_arg[3] = NULL;
+		}
+		else 
+		{
+			exec_arg[1] = lst->argu;
+			exec_arg[2] = NULL;
+		}
 		if (execve(path, exec_arg, env) == -1)
 			return (-2);		//error with execve
 	}
@@ -73,26 +86,11 @@ int		launch_exec(t_list *lst, char **env)
 	return 1;
 }
 
-
-int         execuction_temporaire(t_user *start)
-{
-    void	*ptr;
-	
-    ptr = start->line;
-    while (start->line)
-    {
-		//debug(start->line->content);
-		//dispatch_cmd(start->line->content);
-		//debug(start->line->content);
-		if (launch_exec(start->line->content, start->user_env) == -1)
-			return (-1);
-		//petite_execution(start->line->content, start->user_env);
-        start->line = start->line->next;
-    }
-	
-    start->line = ptr;
-    return (0);
-}
+/*
+** Fonction exectution qui clean les builtins et faire le parsing des flags
+** Actuellement elle n'execute que les premières commandes
+** Tout ce qu'il y a après le '|' est ignoré
+*/
 
 int		 execution(t_user *start)
 {
@@ -106,7 +104,7 @@ int		 execution(t_user *start)
 		debug(lst);
 		clean_builtin(lst);
 		parse_flags(lst);
-		//debug(lst);
+		//dispatch_cmd(lst); je bosse ici actuellement (dans l'implémentation de la gestion des '|')
 		launch_exec(start->line->content, start->user_env);
 		start->line = start->line->next;
 	}
