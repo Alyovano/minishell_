75ece19b8ed6646c61aac4e7a15e3a7111cfeb23
coucou <alyovano@student.s19.be>
2020-11-28 13:27:01 +0100

    [PWD] Fonctionnel + call back en cas de probleme

--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -1,11 +1,23 @@
 #include "../includes/minishell.h"
 
+/*
+**Si buf est NULL lors de l'appel. 
+**Dans ce cas, le tampon alloué a la longueur size à moins que size soit égal à zéro,
+**auquel cas buf est alloué avec la taille nécessaire.
+**Il est possible (et même fortement conseillé) de libérer le tampon avec free(3).
+*/
+
 int         ft_pwd(void)
 {
 	char	*tmp;
 
 	tmp = getcwd(NULL, 0);
+	if (!tmp)
+	{
+		perror("Malloc failure\n");
+		exit(EXIT_FAILURE);
+	}
 	ft_printf("%s\n", tmp);
-	//free(tmp); ? 
+	free(tmp);
     return (0);
 }
--- srcs/exec/dispatch.c
+++ srcs/exec/dispatch.c
@@ -59,7 +59,7 @@ int		dispatch_cmd(t_list *lst, t_env *env)
 	}
 	else if (ft_strcmp("pwd", lst->builtin) == 0)
 	{
-		unset_built(env, lst->argu);
+		ft_pwd();
 	}
 	else if (ft_strcmp("le nom du builtin que tu veux ajouter", lst->builtin) == 0)
 		ft_printf("la fonction builtin que tu apelles\n");
