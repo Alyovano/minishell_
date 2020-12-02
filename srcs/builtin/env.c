43e3f87978a2619fb5861d5ed352fee69f1fcb2c
coucou <alyovano@student.s19.be>
2020-12-02 15:21:01 +0100

    [ENV] Impression des vars qui ont une valeur

--- srcs/builtin/env.c
+++ srcs/builtin/env.c
@@ -1,9 +1,46 @@
 #include "../includes/minishell.h"
 
+/*
+** env ne fait que print les var qui ont une valeur assignee
+** il peut aussi en ajouter ex
+**         $ env lol=56
+** ensuite il print le tableau
+** les valeurs ajoutees par l'apl de env sont detruite apres le print
+*/
+
+/*
+** Check si la var possede une valeur
+*/
+
+int         have_value(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str && str[i])
+    {
+        if (str[i] == '=')
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
 int         ft_env(t_env *env, char *arg)
 {
     (void)env;
     (void)arg;
+    int i;
+
+    i = 0;
+    while (env->tab[i])
+    {
+        if (have_value(env->tab[i]) == 1)
+        {
+            ft_printf("%s\n", env->tab[i]);
+        }
+        i++;
+    }
     printf("COUCOU\n");
   	return (0);
 }
