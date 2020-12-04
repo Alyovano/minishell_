ce39afcd5f40173b90a5480e30ba3c0a14bf383e
coucou <alyovano@student.s19.be>
2020-12-05 00:19:18 +0100

    [LEAKS] Free propre en fin de boucle -> Ajouter un algo de free de la liste chainee

--- srcs/minishell/minishell.c
+++ srcs/minishell/minishell.c
@@ -5,11 +5,23 @@
 ** On attend l'input, on l'envoie dans le parseur
 */
 
-int     end_loop_free(t_user *start, char *str)
+    // ici une boucle qui free toute la liste t_list ?
+    // il ne faut surtout pas free le pointeur *start
+    // car il n'est alloue que dans le main 
+
+int     end_loop_free(t_user *start, char *str, int token_used)
 {
+    int i;
+
+    i = 0;
+    while (token_used == 1 && start->user_cmd_tab[i])
+    {
+        free(start->user_cmd_tab[i]);
+        i++;
+    }
+    if (token_used == 1)
+        free(start->user_cmd_tab);
     free(str);
-    free_double_tab(start->user_cmd_tab);
-    // ici une boucle qui free toute la list t_list ?
     return (0);
 }
 
@@ -24,16 +36,19 @@ void	ft_sig(int value)
 
 int     minishell_loop(t_user *start, t_env *env)
 {
-    char *user_input;
+    int     used;
+    char    *user_input;
 
 	signal(SIGINT, ft_sig);
 	signal(SIGQUIT, ft_sig);
     while (1)
     {
+        used = 0;
         ft_printf("minishell> ");
         get_next_line(0, &user_input);
         if (ft_strcmp(user_input, "") != 0)
         {
+            used = 1;
             if (parsing_input(user_input, start, env) != -1)
             {
                 if (conditionning(start) != -1)
@@ -42,7 +57,7 @@ int     minishell_loop(t_user *start, t_env *env)
                 }
             }
         }
-        end_loop_free(start, user_input);
+        end_loop_free(start, user_input, used);
     }
     return (0);
 }
