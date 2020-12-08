67507a98a923109d82866e16ffb69a80d50b0bdd
coucou <alyovano@student.s19.be>
2020-12-08 18:12:09 +0100

    [EXIT] ft_printf

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -5,11 +5,6 @@ int			ft_exit(t_env *env, t_list *lst, t_user *start)
 	(void)env;
 	(void)lst;
 	(void)start;
-	printf("exit\n");
+	ft_printf("exit\n");
 	exit(EXIT_SUCCESS);
-	// printf("1\n");
-	// exit(EXIT_SUCCESS);
-	// printf("2\n");
-	// exit(EXIT_SUCCESS);
-	// return EXIT_SUCCESS;
 }
