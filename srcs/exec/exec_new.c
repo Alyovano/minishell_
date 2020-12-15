da60726e34e39aabbbe30a491acae54b04a1fd40
lturbang <lturbang@student.s19.be>
2020-12-15 11:59:16 +0100

    [EXEC] debut clean code

--- srcs/exec/exec_new.c
+++ srcs/exec/exec_new.c
@@ -1,70 +1,88 @@
 #include "../includes/minishell.h"
 
-int		exec_redirrect(t_list *lst, t_env *env, int old_fd[2], int size)
-{	
-	t_fd	fds;
+/*
+** (set_input)
+** go trough lst->in file list
+** open file and create if needed
+** redirrect input at the end
+*/
+
+int		set_input(t_list *lst, t_fd *fds, int i)
+{
+	while (lst->in[i])
+	{
+		if (ft_strcmp(lst->in_types[i], "READ") == 0)
+		{
+			fds->fdin = open(lst->in[i], O_RDONLY);
+			if (fds->fdin == -1)
+			{
+				error_output_token(-8, lst->in[i], '\0');
+				return (-1);
+			}
+		}
+		else if (ft_strcmp(lst->in_types[i], "READ_CREATE") == 0)
+			fds->fdin = open(lst->in[i], O_RDONLY | O_CREAT | O_TRUNC, 0664);
+		if (lst->in[i + 1] != NULL)
+			close(fds->fdin);
+		i++;
+	}
+	dup2(fds->fdin, 0);
+	close(fds->fdin);
+	return (0);
+}
+
+/*
+** (set_output)
+** go trough lst->out file list
+** open file and create if needed
+** redirrect output at the end
+*/
+
+void		set_output(t_list *lst, t_fd *fds, int i)
+{
+	while (lst->out[i])
+	{
+		if (ft_strcmp(lst->out_types[i], "WRITE") == 0)
+			fds->fdout = open(lst->out[i], O_WRONLY | O_CREAT | O_TRUNC, 0664);				
+		else if (ft_strcmp(lst->out_types[i], "APPEND") == 0)
+			fds->fdout = open(lst->out[i], O_WRONLY | O_CREAT | O_APPEND, 0664);				
+		if (lst->out[i + 1] != NULL)
+			close(fds->fdout);
+		i++;
+	}
+	dup2(fds->fdout, 1);
+	close(fds->fdout);
+}
+
+/*
+** (in_out_setup)
+** save in / out before
+** if in/out file(s) --> setup in set_output / set_input
+*/
+
+int		in_out_setup(t_fd *fds, t_list *lst)
+{
 	int 	i;
 
 	i = 0;
-	//save in / out
-	fds.tmpin = dup(0);
-	fds.tmpout = dup(1);
-	//setup pipe for no file use
+	fds->tmpin = dup(0);
+	fds->tmpout = dup(1);
 	if (lst->in[0] == NULL || lst->out[0] == NULL)
 		pipe(lst->fd);
-	//setup input with file
 	if (lst->in[0] != NULL)
-	{
-		while (lst->in[i]) //input from redirrections
-		{
-			if (ft_strcmp(lst->in_types[i], "READ") == 0)
-			{
-				ft_printf("READ\n");
-				fds.fdin = open(lst->in[i], O_RDONLY);
-				if (fds.fdin == -1)
-				{
-					error_output_token(-8, lst->in[i], '\0');
-					return (-1);
-				}
-			}
-			else if (ft_strcmp(lst->in_types[i], "READ_CREATE") == 0)
-			{
-				ft_printf("READ_CREATE\n");
-				fds.fdin = open(lst->in[i], O_RDONLY | O_CREAT | O_TRUNC, 0664);
-			}
-			if (lst->in[i + 1] != NULL)
-				close(fds.fdin);
-			i++;
-		}
-		//redirret input
-		dup2(fds.fdin, 0);
-		close(fds.fdin);
-	}
-	
-	//setup output with file
-	i = 0;
+		if (set_input(lst, fds, i) == -1)
+			return (-1);
 	if (lst->out[0] != NULL)
-	{
-		while (lst->out[i]) //input from redirrections
-		{
-			if (ft_strcmp(lst->out_types[i], "WRITE") == 0)
-			{
-				ft_printf("Write\n");
-				fds.fdout = open(lst->out[i], O_WRONLY | O_CREAT | O_TRUNC, 0664);				
-			}
-			else if (ft_strcmp(lst->out_types[i], "APPEND") == 0)
-			{
-				ft_printf("APPEND\n");
-				fds.fdout = open(lst->out[i], O_WRONLY | O_CREAT | O_APPEND, 0664);				
-			}
-			if (lst->out[i + 1] != NULL)
-				close(fds.fdout);
-			i++;
-		}
-		//redirrect output
-		dup2(fds.fdout, 1);
-		close(fds.fdout);
-	}
+		set_output(lst, fds, i);
+	return (0);
+}
+
+int		exec_redirrect(t_list *lst, t_env *env, int old_fd[2], int size)
+{	
+	t_fd	fds;
+
+	if (in_out_setup(&fds, lst) == -1)
+		return (-1);
 	if (size == 1 && (ft_strcmp("export", lst->builtin) == 0 || ft_strcmp("cd", lst->builtin) == 0 \
 					|| ft_strcmp("unset", lst->builtin) == 0))
 	{
@@ -89,31 +107,18 @@ int		exec_redirrect(t_list *lst, t_env *env, int old_fd[2], int size)
 					dup2(old_fd[0], STDIN_FILENO);
 					close(old_fd[1]);
 				}
-			}
-			
-			//fork
-			//lst->pid = fork();
-			
+			}			
 			dispatch_cmd(lst, env);
 			perror("EXECVP");
 			exit(1);
 		}
 	}
-	
 	if (lst->out[0] == NULL || lst->in[0] == NULL)
 		close(lst->fd[1]);
 	dup2(fds.tmpin, 0);
 	dup2(fds.tmpout, 1);
 	close(fds.tmpin);
 	close(fds.tmpout);
-	
-
-	/*
-	while (lst)
-	{
-		ft_printf("BUILIN: %s\n", lst->builtin);
-		lst = lst->next;
-	}*/
 	return (1);
 }
 
