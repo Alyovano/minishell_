2aa541ae4a537f1af2e1d7c33b17024855d98df4
lturbang <lturbang@student.s19.be>
2021-01-04 13:39:23 +0100

    [redirrect.c] NORMED

--- srcs/redirrect/redirrect.c
+++ srcs/redirrect/redirrect.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirrect.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/04 12:51:22 by user42            #+#    #+#             */
+/*   Updated: 2021/01/04 13:35:50 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
 int		is_redirrect(t_list *lst, t_quote *quote)
 {
 	char	*tmp;
 	int		i;
-	
+
 	i = 0;
 	tmp = ft_strdup(lst->content);
 	quote->token_in_dquote = -1;
@@ -15,8 +27,9 @@ int		is_redirrect(t_list *lst, t_quote *quote)
 			quote->token_in_simple_quote *= -1;
 		else if (tmp[i] == '"' && get_backslash(tmp, i) == 0)
 			quote->token_in_dquote *= -1;
-		else if ((tmp[i] == '>' || tmp[i] == '<') && get_backslash(tmp, i) == 0 
-			&& quote->token_in_dquote == -1 && quote->token_in_simple_quote == -1)
+		else if ((tmp[i] == '>' || tmp[i] == '<') && get_backslash(tmp, i) == 0
+			&& quote->token_in_dquote == -1 &&\
+					quote->token_in_simple_quote == -1)
 			return (1);
 		i++;
 	}
@@ -40,6 +53,56 @@ void	next_quote(char *str, int *i)
 	}
 }
 
+int		check_next_redirrect(int *i, char *tmp)
+{
+	if (tmp[*i] == '>' && tmp[*i + 1] == '>')
+	{
+		if (check_redirrect(tmp, *i + 2) == -1)
+			return (-1);
+	}
+	else if (tmp[*i] == '<' && tmp[*i + 1] == '>')
+	{
+		if (check_redirrect(tmp, *i + 2) == -1)
+			return (-1);
+	}
+	else if (tmp[*i] == '>')
+	{
+		if (check_redirrect(tmp, *i + 1) == -1)
+			return (-1);
+	}
+	else if (tmp[*i] == '<')
+	{
+		if (check_redirrect(tmp, *i + 1) == -1)
+			return (-1);
+	}
+	return (0);
+}
+
+int		get_redirrect2(int *i, char *tmp, t_list *lst)
+{
+	if (check_next_redirrect(i, tmp) == -1)
+		return (-1);
+	if (tmp[*i] == '>' && tmp[*i + 1] == '>')
+	{
+		(*i) += write_redirrect(tmp, *i + 2, lst, "APPEND");
+		(*i)++;
+	}
+	else if (tmp[*i] == '<' && tmp[*i + 1] == '>')
+	{
+		(*i) += read_redirrect(tmp, *i + 2, lst, "READ_CREATE");
+		(*i)++;
+	}
+	else if (tmp[*i] == '>')
+	{
+		(*i) += write_redirrect(tmp, *i + 1, lst, "WRITE");
+	}
+	else if (tmp[*i] == '<')
+	{
+		(*i) += read_redirrect(tmp, *i + 1, lst, "READ");
+	}
+	return (0);
+}
+
 int		get_redirrect(t_list *lst, t_quote *quote)
 {
 	char	*tmp;
@@ -50,7 +113,7 @@ int		get_redirrect(t_list *lst, t_quote *quote)
 	quote->token_in_dquote = -1;
 	quote->token_in_simple_quote = -1;
 	if (redirrect_error(tmp) == -1)
-		return (-1); 
+		return (-1);
 	while (tmp[i])
 	{
 		if ((tmp[i] == '\'' || tmp[i] == '"') && get_backslash(tmp, i) == 0)
@@ -59,33 +122,8 @@ int		get_redirrect(t_list *lst, t_quote *quote)
 		}
 		else if ((tmp[i] == '>' || tmp[i] == '<') && get_backslash(tmp, i) == 0)
 		{
-			//algo redirrections
-			if (tmp[i] == '>' && tmp[i + 1] == '>')
-			{
-				if (check_redirrect(tmp, i + 2) == -1)
-					return (-1);
-				i += write_redirrect(tmp, i + 2, lst, "APPEND");
-				i++;
-			}
-			else if (tmp[i] == '<' && tmp[i + 1] == '>')
-			{
-				if (check_redirrect(tmp, i + 2) == -1)
-					return (-1);
-				i += read_redirrect(tmp, i + 2, lst, "READ_CREATE");
-				i++;
-			}
-			else if (tmp[i] == '>')
-			{
-				if (check_redirrect(tmp, i + 1) == -1)
-					return (-1);
-				i += write_redirrect(tmp, i + 1, lst, "WRITE");
-			}
-			else if (tmp[i] == '<')
-			{
-				if (check_redirrect(tmp, i + 1) == -1)
-					return (-1);
-				i += read_redirrect(tmp, i + 1, lst, "READ");
-			}
+			if (get_redirrect2(&i, tmp, lst) == -1)
+				return (-1);
 		}
 		i++;
 	}
