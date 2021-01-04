e7245ab857e32bdca2c4379d45c6266a07160acd
coucou <alyovano@student.s19.be>
2021-01-04 19:04:38 +0100

    First_split.c norme (Encore une fonction a couper) :

--- srcs/parsing/first_split.c
+++ srcs/parsing/first_split.c
@@ -1,105 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean_line.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/20 10:26:17 by user42            #+#    #+#             */
+/*   Updated: 2020/10/22 13:20:04 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
-char	*ft_str_n_dup(const char *s1, int size)
+char		*ft_str_n_dup(const char *s1, int size)
 {
 	size_t	longueur;
 	char	*tab;
 
 	longueur = size + 1;
 	tab = malloc(sizeof(char) * longueur);
-    if (tab == NULL)
-    {
-        perror("Malloc Failure\n");
-        exit(EXIT_FAILURE);
-    }
+	if (tab == NULL)
+		malloc_error();
 	ft_strlcpy(tab, s1, longueur);
 	return (tab);
 }
 
-int         init_double_tab_cmd(t_user *start)
+int			init_double_tab_cmd(t_user *start)
 {
-    int i;
+	int i;
 
-    i = 0;
-    start->user_cmd_tab = malloc(sizeof(start->user_cmd_tab) * (start->split_nb + 1));
-    if (!start->user_cmd_tab)
-        return (-1);
-    while(i < start->split_nb + 1)
-    {
-        start->user_cmd_tab[i] = 0;
-        i++;
-    }
-    return (0);
+	i = 0;
+	start->user_cmd_tab = malloc(sizeof(start->user_cmd_tab)
+					* (start->split_nb + 1));
+	if (!start->user_cmd_tab)
+		return (-1);
+	while (i < start->split_nb + 1)
+	{
+		start->user_cmd_tab[i] = 0;
+		i++;
+	}
+	return (0);
 }
 
-int         maybe_split(t_user *start, int i)
+int			maybe_split(t_user *start, int i)
 {
-    if (start->user_input[i] == ';')
-    {
-        if (get_backslash(start->user_input, i) == 0)
-            return (0);
-    }
-    return (1);
+	if (start->user_input[i] == ';')
+	{
+		if (get_backslash(start->user_input, i) == 0)
+			return (0);
+	}
+	return (1);
 }
 
+/*
+** FIX
+** Il faudra tout reverifier encore une fois, ligne par ligne
+** directement a l'interieur du tableau, des erreurs peuvent encore passer
+** + norme |!|
+*/
 
-// FIX 
-// Il faudra tout reverifier encore une fois, ligne par ligne
-// directement a l'interieur du tableau, des erreurs peuvent encore passer
-// + norme |!|
-
-void         init_quotes_to_fix(t_quote *quote)
+void			init_quotes_to_fix(t_quote *quote)
 {
-        quote->verif = 0;
-        quote->t_in_squote = 0;
-        quote->t_in_dquote = 0;
+	quote->verif = 0;
+	quote->t_in_squote = 0;
+	quote->t_in_dquote = 0;
 }
 
-int         cut_input_to_tab(t_user *start, t_quote *quote)
+int				cut_input_to_tab(t_user *start, t_quote *quote)
 {
-    int k = 0;
-    int i = 0;
-    int j = 0;
+	int k;
+	int i;
+	int j;
 
-    while (start->user_input[i])
-    {
-        init_quotes_to_fix(quote);
-        if (start->user_input[i] == '\'' 
-        && (get_backslash(start->user_input, i) == 0))
-        {
-            quote->t_in_squote = 1;
-            quote_get_len_and_validity(start, quote, i + 1);
-            i += quote->len;
-            quote->t_in_squote = 0;
-        }
-        if (start->user_input[i] == '"' 
-        && (get_backslash(start->user_input, i) == 0))
-        { 
-            quote->t_in_dquote = 1;
-            quote_get_len_and_validity(start, quote, i + 1);
-            i += quote->len;
-            quote->t_in_dquote = 0;
-        }
-        if (maybe_split(start, i) == 0)
-        {
-            start->user_cmd_tab[k] = ft_str_n_dup(start->user_input + j, i - j);
-            k++;
-            j = i;
-        }
-        i++;
-    }
-    if (start->user_input[i] == 0 && quote->verif == 0)
-    {
-        start->user_cmd_tab[k] = ft_str_n_dup(start->user_input + j, i - j);
-        k++;
-    }
-    return (0);
+	k = 0;
+	i = 0;
+	j = 0;
+	while (start->user_input[i])
+	{
+		init_quotes_to_fix(quote);
+		if (start->user_input[i] == '\''
+		&& (get_backslash(start->user_input, i) == 0))
+		{
+			quote->t_in_squote = 1;
+			quote_get_len_and_validity(start, quote, i + 1);
+			i += quote->len;
+			quote->t_in_squote = 0;
+		}
+		if (start->user_input[i] == '"'
+		&& (get_backslash(start->user_input, i) == 0))
+		{
+			quote->t_in_dquote = 1;
+			quote_get_len_and_validity(start, quote, i + 1);
+			i += quote->len;
+			quote->t_in_dquote = 0;
+		}
+		if (maybe_split(start, i) == 0)
+		{
+			start->user_cmd_tab[k] = ft_str_n_dup(start->user_input + j, i - j);
+			k++;
+			j = i;
+		}
+		i++;
+	}
+	if (start->user_input[i] == 0 && quote->verif == 0)
+	{
+		start->user_cmd_tab[k] = ft_str_n_dup(start->user_input + j, i - j);
+		k++;
+	}
+	return (0);
 }
 
-int         first_split_dirty_line(t_user *start, t_quote *quote)
+int				first_split_dirty_line(t_user *start, t_quote *quote)
 {
-    if (init_double_tab_cmd(start) == -1)
-        return (-1);
-    cut_input_to_tab(start, quote);
-    return (0);
+	if (init_double_tab_cmd(start) == -1)
+		return (-1);
+	cut_input_to_tab(start, quote);
+	return (0);
 }
