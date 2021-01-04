83a16be95740f51c977c9ed27f285968748e7018
coucou <alyovano@student.s19.be>
2021-01-04 18:52:59 +0100

    Correction de norme et depart de 0

--- srcs/parsing/dollar.c
+++ srcs/parsing/dollar.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean_spaces.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/21 10:45:57 by user42            #+#    #+#             */
+/*   Updated: 2021/01/04 13:57:31 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
-char	*ft_strn_dup(const char *s1, int len)
+char		*ft_strn_dup(const char *s1, int len)
 {
 	size_t	longueur;
 	char	*tab;
@@ -12,35 +24,36 @@ char	*ft_strn_dup(const char *s1, int len)
 	return (tab);
 }
 
-
-int simple_quote_get_len_and_validity(t_user *start, t_quote *quote, int i, int j)
+int			simple_quote_get_len_and_validity(t_user *start,
+								t_quote *quote, int i, int j)
 {
-    quote->len = 0;
-    while (start->user_cmd_tab[i][j]
-    && (quote->t_in_squote % 2 != 0 || quote->t_in_dquote % 2 != 0))
-    {
-        if (start->user_cmd_tab[i][j] == '"')
-        {
-            if (get_backslash(start->user_cmd_tab[i], j) == 0 && quote->t_in_squote % 2 == 0)
-                quote->t_in_dquote++;
-        }
-        else if (start->user_cmd_tab[i][j] == '\'')
-        {
-            if (quote->t_in_squote % 2 == 0 && quote->t_in_dquote % 2 == 0)
-            {
-                if (get_backslash(start->user_cmd_tab[i], j) == 0)
-                    quote->t_in_squote++;
-            }
-            else if (quote->t_in_squote % 1 == 0 && quote->t_in_dquote % 2 == 0)
-                quote->t_in_squote++;
-        }
-        j++;
-        quote->len++;
-    }
-    return (quote->len - 1);
+	quote->len = 0;
+	while (start->user_cmd_tab[i][j]
+	&& (quote->t_in_squote % 2 != 0 || quote->t_in_dquote % 2 != 0))
+	{
+		if (start->user_cmd_tab[i][j] == '"')
+		{
+			if (get_backslash(start->user_cmd_tab[i], j) == 0
+			&& quote->t_in_squote % 2 == 0)
+				quote->t_in_dquote++;
+		}
+		else if (start->user_cmd_tab[i][j] == '\'')
+		{
+			if (quote->t_in_squote % 2 == 0 && quote->t_in_dquote % 2 == 0)
+			{
+				if (get_backslash(start->user_cmd_tab[i], j) == 0)
+					quote->t_in_squote++;
+			}
+			else if (quote->t_in_squote % 1 == 0 && quote->t_in_dquote % 2 == 0)
+				quote->t_in_squote++;
+		}
+		j++;
+		quote->len++;
+	}
+	return (quote->len - 1);
 }
 
-/*	
+/*
 **  ce token (quote->dollar_quote) permet de savoir
 **  si on est pas deja dans une doucle quote
 **	pcq si c'est le cas meme entre simple quote, il faudrait afficher
@@ -49,69 +62,85 @@ int simple_quote_get_len_and_validity(t_user *start, t_quote *quote, int i, int 
 **	output> "'alyovano'"
 */
 
-int         check_simple_quote(t_user *start, t_quote *quote, int j, int i)
+int			free_dol(t_dollar *dol, char *one, char *two)
 {
-    if (start->user_cmd_tab[i][j] == '\'' 
-    && (get_backslash(start->user_cmd_tab[i], j) == 0) && quote->dollar_quote % 2 == 0)
-    {
-        quote->t_in_squote = 1;
-        simple_quote_get_len_and_validity(start, quote, i, j + 1);
-        j += quote->len;
-        quote->t_in_squote = 0;
-    }
+	if (dol->var_content)
+		free(dol->var_content);
+	if (dol->before_str)
+		free(dol->before_str);
+	if (dol->after_str)
+		free(dol->after_str);
+	if (one)
+		free(one);
+	if (two)
+		free(two);
+	return (0);
+}
+
+int			check_simple_quote(t_user *start, t_quote *quote, int j, int i)
+{
+	if (start->user_cmd_tab[i][j] == '\''
+	&& (get_backslash(start->user_cmd_tab[i], j) == 0)
+	&& quote->dollar_quote % 2 == 0)
+	{
+		quote->t_in_squote = 1;
+		simple_quote_get_len_and_validity(start, quote, i, j + 1);
+		j += quote->len;
+		quote->t_in_squote = 0;
+	}
 	if (start->user_cmd_tab[i][j] == '"'
-    && (get_backslash(start->user_cmd_tab[i], j) == 0))
+	&& (get_backslash(start->user_cmd_tab[i], j) == 0))
 	{
 		quote->dollar_quote += 1;
 	}
-    return (j);
+	return (j);
 }
 
-int         str_check(char *str_envi, char *to_catch)
+int			str_check(char *str_envi, char *to_catch)
 {
-    int i;
-    int len;
-    int check;
+	int i;
+	int len;
+	int check;
 
-    i = 0;
-    check = 0;
-    len = ft_strlen(to_catch);
-    while (i < len)
-    {
-        if (to_catch[i] == str_envi[i])
-        {
-            check++;
-        }
-        i++;
-    }
-    if (check == len && str_envi[i] == '=')
-        return (0);
-    return (1);
+	i = 0;
+	check = 0;
+	len = ft_strlen(to_catch);
+	while (i < len)
+	{
+		if (to_catch[i] == str_envi[i])
+		{
+			check++;
+		}
+		i++;
+	}
+	if (check == len && str_envi[i] == '=')
+		return (0);
+	return (1);
 }
 
-char        *check_var_in_env(char *var_name, t_env *env)
+char		*check_var_in_env(char *var_name, t_env *env)
 {
-    int i;
-    int token_copy;
-    char *new;
+	int		i;
+	int		token_copy;
+	char	*new;
 
-    i = 0;
-    token_copy = 0;
-    while (env->tab[i])
-    {
-        if (str_check(env->tab[i], var_name) == 0)
-        {
-            if (env->tab[i])
-                token_copy = 1;
-            break ;
-        }
-        i++;
-    }
-    if (token_copy == 1)
-        new = ft_substr(env->tab[i], ft_strlen(var_name) + 1, ft_strlen(env->tab[i]));
-    else
-        new = ft_strdup("");
-    return (new);
+	i = 0;
+	token_copy = 0;
+	while (env->tab[i])
+	{
+		if (str_check(env->tab[i], var_name) == 0)
+		{
+			if (env->tab[i])
+				token_copy = 1;
+			break ;
+		}
+		i++;
+	}
+	if (token_copy == 1)
+		new = ft_substr(env->tab[i], ft_strlen(var_name) + 1, ft_strlen(env->tab[i]));
+	else
+		new = ft_strdup("");
+	return (new);
 }
 
 /*
@@ -121,76 +150,90 @@ char        *check_var_in_env(char *var_name, t_env *env)
 */
 
 // ne pas oublier de placer les free ici
-int        dollar_var_name(t_user *start, int i, int j, t_dollar *dol, t_env *env)
+int			dollar_var_name(t_user *start, int i, int j, t_dollar *dol, t_env *env)
 {
 	int tmp;
 	int k;
 	char *one;
 	char *two;
 
-    j += 1;
+	j += 1;
 	tmp = j;
 	k = 0;
-    while (start->user_cmd_tab[i][j + k] && start->user_cmd_tab[i][j + k] != ' ' 
-    && start->user_cmd_tab[i][j + k] != '\'' && start->user_cmd_tab[i][j + k] != '"' 
-    && start->user_cmd_tab[i][j + k] != '=' && start->user_cmd_tab[i][j + k] != '.')
-	{
-        k++;
-	}
+	while (start->user_cmd_tab[i][j + k] && start->user_cmd_tab[i][j + k] != ' '
+	&& start->user_cmd_tab[i][j + k] != '\'' && start->user_cmd_tab[i][j + k] != '"'
+	&& start->user_cmd_tab[i][j + k] != '=' && start->user_cmd_tab[i][j + k] != '.')
+		k++;
 	dol->var_name = ft_substr(start->user_cmd_tab[i], tmp, k);
-    dol->var_content = check_var_in_env(dol->var_name, env);
+	dol->var_content = check_var_in_env(dol->var_name, env);
 	dol->before_str = ft_substr(start->user_cmd_tab[i], 0, j - 1);
 	dol->after_str = ft_substr(start->user_cmd_tab[i], j + k, ft_strlen(start->user_cmd_tab[i]));
 	one = ft_strjoin(dol->before_str, dol->var_content);
 	two = ft_strjoin(one, dol->after_str);
-	start->user_cmd_tab[i] = two;
+	free(start->user_cmd_tab[i]);
+	start->user_cmd_tab[i] = ft_strdup(two);
 	tmp = ft_strlen(one);
-    return (tmp);
+	//free_dol(dol, one, two);
+	// if (dol->var_name != NULL)
+	// 	free(dol->var_name);
+	return (tmp);
 }
 
 /*
 ** j = check_simple_quote(start, quote, j, i);// jump sur la char apres la squote
 */
 
-int         check_dollar_or_not_dollar(t_user *start,
-                     t_quote *quote, int i, t_dollar *dol, t_env *env)
+int				check_dollar_or_not_dollar(t_user *start,
+						t_quote *quote, int i, t_dollar *dol, t_env *env)
 {
-    int j;
+	int j;
 
-    j = 0;
+	j = 0;
 	quote->dollar_quote = 0;
-    while (start->user_cmd_tab[i][j])
-    {
-        j = check_simple_quote(start, quote, j, i);
-        if (start->user_cmd_tab[i][j] == '$' && 
-                    (get_backslash(start->user_cmd_tab[i], j) == 0) 
-                    && (start->user_cmd_tab[i][j + 1]) 
-                    && (start->user_cmd_tab[i][j + 1] != ' ') 
-                    && (start->user_cmd_tab[i][j + 1] != '"'))
-        {
-            j = dollar_var_name(start, i, j, dol, env);
-        }
-        j++;
-    }
-    return (0);
+	while (start->user_cmd_tab[i][j])
+	{
+		j = check_simple_quote(start, quote, j, i);
+		if (start->user_cmd_tab[i][j] == '$' &&
+					(get_backslash(start->user_cmd_tab[i], j) == 0)
+					&& (start->user_cmd_tab[i][j + 1])
+					&& (start->user_cmd_tab[i][j + 1] != ' ')
+					&& (start->user_cmd_tab[i][j + 1] != '"'))
+		{
+			if (start->user_cmd_tab[i][j + 1] && (start->user_cmd_tab[i][j + 1] == '?'))
+			{
+				// ici il faut implementer $?
+				j = printf("%d\n", errno);
+			}
+			else
+			{
+				j = dollar_var_name(start, i, j, dol, env);
+			}
+		}
+		j++;
+	}
+	return (0);
 }
 
-int         add_environnement_var(t_user *start, t_quote *quote, t_env *env)
-{
-    int i;
+/*
+** PLACER LES FREE
+** free(dol) avant le return
+*/
 
-	t_dollar *dol;
-    if (!(dol = malloc(sizeof(dol))))
-	{
-        return (-1);
-	}
+int				add_environnement_var(t_user *start, t_quote *quote, t_env *env)
+{
+	int			i;
+	t_dollar	*dol;
+
+	dol = malloc(sizeof(dol));
+	if (!dol)
+		malloc_error();
 	i = 0;
 	dol->start_cut = 0;
-    while  (start->user_cmd_tab[i])
-    {
-        check_dollar_or_not_dollar(start, quote, i, dol, env);
-        i++;
-    }
-    //free(dol);
-    return (0);
+	while (start->user_cmd_tab[i])
+	{
+		check_dollar_or_not_dollar(start, quote, i, dol, env);
+		i++;
+	}
+	free(dol);
+	return (0);
 }
