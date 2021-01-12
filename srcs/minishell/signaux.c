4c9caa9e558ab88122d0c05d9406960b50e85ed8
Aly <alyovano@student.s19.be>
2021-01-13 00:14:47 +0100

    Signaux.c Norme

--- srcs/minishell/signaux.c
+++ srcs/minishell/signaux.c
@@ -15,12 +15,10 @@
 void		sig_handler(int sig)
 {
 	(void)sig;
-	//char buf[PATH_CWD_MAX + 1];
-
 	wait(NULL);
 	ft_putchar_fd('\n', 1);
 	if (g_reg != 1)
-		ft_printf("Minishell> ");	//print_promt(getcwd(buf, sizeof(buf)));
+		ft_printf("Minishell> ");
 	g_reg = -1;
 }
 
@@ -36,7 +34,7 @@ void		catch_signal(void)
 int			verif_ret(int ret, char *user_input)
 {
 	(void)user_input;
-	if (ret == 0)
+	if (ret == 0 && user_input[0] == 0)
 	{
 		write(1, "exit\n", 5);
 		exit(EXIT_SUCCESS);
