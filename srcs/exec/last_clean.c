bcd8829aa0868d42a0d55f7f6cbb8d7fd5ccad2f
lturbang <lturbang@student.s19.be>
2021-01-12 13:44:12 +0100

    [last_clean] fix bug with \" + norme

--- srcs/exec/last_clean.c
+++ srcs/exec/last_clean.c
@@ -6,101 +6,102 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 09:47:44 by user42            #+#    #+#             */
-/*   Updated: 2021/01/12 12:43:50 by user42           ###   ########.fr       */
+/*   Updated: 2021/01/12 13:42:23 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-char		*realloc_clean(char *str, t_quote *q, int size)
+int		realloc_clean2(char *str, char **ret, int *j, t_quote *q)
+{
+	int i;
+
+	i = 0;
+	if (str[i] == '\'' && get_backslash(str, i) == 0 && q->dquote == -1)
+	{
+		q->squote *= -1;
+		i++;
+		while (str[i] && q->squote == 1 && str[i] != '\'')
+			(*ret)[(*j)++] = str[i++];
+	}
+	else if (str[i] == '\\')
+	{
+		if (q->dquote == -1)
+			(*ret)[(*j)++] = str[++i];
+		else if (!(q->dquote == 1 && (str[i + 1] == '\\' || \
+						str[i + 1] == '$' || str[i + 1] == '"')))
+			(*ret)[(*j)++] = str[i];
+	}
+	else
+		(*ret)[(*j)++] = str[i];
+	return (i);
+}
+
+char	*realloc_clean(char *str, int size)
 {
 	int		i;
 	int		j;
-	int len = 0;
 	char	*ret;
+	t_quote	q;
 
-	i = 0;
+	i = -1;
 	j = 0;
-	q->squote = -1;
-	q->dquote = -1;
+	q.squote = -1;
+	q.dquote = -1;
 	ret = malloc(sizeof(char) * (size + 1));
 	if (!ret)
 		malloc_error();
-	while (str[i])
+	while (str[++i])
 	{
 		if (str[i] == '"' && get_backslash(str, i) == 0)
-		{
-			q->dquote *= -1;
-			len--;
-		}
-		else if (str[i] == '\'' && get_backslash(str, i) == 0 && q->dquote == -1)
-		{
-			q->squote *= -1;
-			len--;
-			i++;
-			while (str[i] && q->squote == 1 && str[i] != '\'')
-				ret[j++] = str[i++];
-			len--;
-			
-		}
-		else if (str[i] == '\\')
-		{
-			if (q->dquote == -1)
-			 {
-				len--;
-				ret[j++] = str[++i];
-			 }				
-			else if (q->dquote == 1 && (str[i + 1] == '\\' || str[i + 1] == '$'))
-				len--;
-			else
-				ret[j++] = str[i];
-		}
+			q.dquote *= -1;
 		else
-			ret[j++] = str[i];
-		i++;
+			i += realloc_clean2(str + i, &ret, &j, &q);
 	}
 	ret[j] = '\0';
 	return (ret);
 }
 
-int		get_nb_clean(char *str, t_quote *q)
+void	get_nb_clean2(char *str, t_quote *q, int *i, int *len)
 {
-	int i;
-	int len;
-
-	i = 0;
-	len = ft_strlen(str);
-	q->squote = -1;
-	q->dquote = -1;
-	while (str[i])
+	if (str[*i] == '"' && get_backslash(str, *i) == 0)
 	{
-		if (str[i] == '"' && get_backslash(str, i) == 0)
-		{
-			q->dquote *= -1;
-			len--;
-		}
-		else if (str[i] == '\'' && get_backslash(str, i) == 0)
-		{
-			q->squote *= -1;
-			len--;
-			i++;
-			while (str[i] && q->squote == 1 && str[i] != '\'')
-				i++;
-			len--;
-			
-		}
-		else if (str[i] == '\\')
-		{
-			if (q->dquote == -1)
-			 {
-				len--;
-				i++;
-			 }				
-			else if (q->dquote == 1 && (str[i + 1] == '\\' || str[i + 1] == '$'))
-				len--;
-		}
-		i++;
+		q->dquote *= -1;
+		(*len)--;
 	}
+	else if (str[*i] == '\'' && get_backslash(str, *i) == 0)
+	{
+		q->squote *= -1;
+		*len -= 2;
+		(*i)++;
+		while (str[*i] && q->squote == 1 && str[*i] != '\'')
+			(*i)++;
+	}
+	else if (str[*i] == '\\')
+	{
+		if (q->dquote == -1)
+		{
+			(*len)--;
+			(*i)++;
+		}
+		else if (q->dquote == 1 && (str[*i + 1] == '\\' || str[*i + 1] == '$' \
+													|| str[*i + 1] == '"'))
+			(*len)--;
+	}
+}
+
+int		get_nb_clean(char *str)
+{
+	int		i;
+	int		len;
+	t_quote	q;
+
+	i = -1;
+	len = ft_strlen(str);
+	q.squote = -1;
+	q.dquote = -1;
+	while (str[++i])
+		get_nb_clean2(str, &q, &i, &len);
 	return (len);
 }
 
@@ -109,27 +110,24 @@ int		last_clean(t_list *lst)
 	int		i;
 	char	*tmp;
 	int		size;
-	t_quote	q;
 
 	while (lst)
 	{
-		i = 0;
-		while (lst->tab_cmd[i])
+		i = -1;
+		while (lst->tab_cmd[++i])
 		{
-			//clean
-			if (find_char(lst->tab_cmd[i], '"') || find_char(lst->tab_cmd[i], '\'') \
-										|| find_char(lst->tab_cmd[i], '\\'))
+			if (find_char(lst->tab_cmd[i], '"') || \
+		find_char(lst->tab_cmd[i], '\'') || find_char(lst->tab_cmd[i], '\\'))
 			{
-				size = get_nb_clean(lst->tab_cmd[i], &q);
+				size = get_nb_clean(lst->tab_cmd[i]);
 				if ((size_t)size != ft_strlen(lst->tab_cmd[i]))
 				{
-					tmp = realloc_clean(lst->tab_cmd[i], &q, size);
+					tmp = realloc_clean(lst->tab_cmd[i], size);
 					free(lst->tab_cmd[i]);
 					lst->tab_cmd[i] = ft_strdup(tmp);
 					free(tmp);
 				}
 			}
-			i++;
 		}
 		lst = lst->next;
 	}
